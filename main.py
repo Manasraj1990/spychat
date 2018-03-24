4f6a3e319bbcbe47a5a378c3ac6583eeb7b5c776
manasraj <manasrajrastogi78@gmail.com>
2018-03-24 22:09:15 +0530

    spychat first project

---
+++ main.py
@@ -0,0 +1,7 @@
+print'Hello Buddy'
+print 'let\'s get started'
+spy_name = raw_input('what is your spy name? ')
+print 'welcome '+ spy_name + ' glad to have you back with us .'
+spy_salutation =raw_input( 'what should we call you (Mr. or Ms.)?')
+spy_name= spy_salutation + ' ' + spy_name
+print spy_name
